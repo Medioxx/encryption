7af36abadcf5c2c03f81e5b525f252a811ba01f3
Marek <marek1cz@gmail.com>
2017-09-07 01:44:16 +0200

    small changes

--- src/Main/mainclass.java
+++ src/Main/mainclass.java
@@ -1,15 +1,12 @@
 package Main;
 
 import Window.mainWindow;
-import Window.resultWindow;
 
 public class mainclass {
 
 	public static void main(String[] args) {
 		mainWindow p=  new mainWindow();
-		resultWindow s= new resultWindow("test", "kocham nuttele");
 		p.setVisible(true);
-		s.setVisible(true);
 		System.out.println(p);
 
 	}
